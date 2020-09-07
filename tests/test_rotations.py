94285f092f89dead63dc364f890089bab49616fa
Nikolas Markou <nikolasmarkou@gmail.com>
2020-09-07 14:33:34 +0300

    Added extra rotation tests

--- tests/test_rotations.py
+++ tests/test_rotations.py
@@ -5,11 +5,10 @@ import numpy as np
 
 def _forward_backwards_rotation_matrix(dims, angle):
     def _test():
-        dims_2 = dims * dims
-        random_points = np.random.randint(0, dims_2, 2)
+        random_points = np.random.randint(0, dims, 2, dtype=np.int)
 
-        zeros_plus = np.zeros((dims, dims))
-        zeros_minus = np.zeros((dims, dims))
+        zeros_plus = np.zeros((dims, dims), dtype=np.float)
+        zeros_minus = np.zeros((dims, dims), dtype=np.float)
 
         x = random_points[0]
         y = random_points[1]
@@ -18,16 +17,16 @@ def _forward_backwards_rotation_matrix(dims, angle):
         zeros_minus[x][y] -= angle
 
         rotation_matrix_plus_pi_4 = crystal.create_rotation_matrix(
-            zeros_plus, cutoff_decimals=5)
+            zeros_plus, cutoff_decimals=10)
 
         rotation_matrix_minus_pi_4 = crystal.create_rotation_matrix(
-            zeros_minus, cutoff_decimals=5)
+            zeros_minus, cutoff_decimals=10)
 
         result = np.matmul(
             rotation_matrix_plus_pi_4,
             rotation_matrix_minus_pi_4)
-        identity_2x2 = np.identity(2)
-        assert np.sum(result - identity_2x2) == pytest.approx(0, 0.001)
+        identity = np.identity(dims, dtype=np.float)
+        assert np.sum(result - identity) == pytest.approx(0., abs=0.0001)
     return _test
 
 
@@ -118,4 +117,19 @@ def test_rotation_forward_backwards_dims100_pi3():
 
 def test_rotation_forward_backwards_dims100_pi4():
     t = _forward_backwards_rotation_matrix(100, np.pi/4)
+    t()
+
+
+def test_rotation_forward_backwards_dims1000_pi2():
+    t = _forward_backwards_rotation_matrix(1000, np.pi/2)
+    t()
+
+
+def test_rotation_forward_backwards_dims1000_pi3():
+    t = _forward_backwards_rotation_matrix(1000, np.pi/3)
+    t()
+
+
+def test_rotation_forward_backwards_dims1000_pi4():
+    t = _forward_backwards_rotation_matrix(1000, np.pi/4)
     t()
